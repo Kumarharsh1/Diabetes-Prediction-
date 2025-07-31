f52f20d04578ae807f3bd9cd823e354c08055f98
KUMAR HARSH <93419195+Kumarharsh1@users.noreply.github.com>
2025-07-31 18:22:06 +0530

    streamlit_app.py

--- diabetes-app/streamlit_app.py
+++ diabetes-app/streamlit_app.py
@@ -15,7 +15,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
-pip install streamlit
+
 """Data Collection and Analysis
 
 PIMA Diabetes Dataset
