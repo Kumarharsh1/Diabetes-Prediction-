0ffba8b78eae6f255289bd84702c395c02d6dd51
KUMAR HARSH <93419195+Kumarharsh1@users.noreply.github.com>
2025-07-31 18:20:35 +0530

    streamlit_app.py

--- diabetes-app/streamlit_app.py
+++ diabetes-app/streamlit_app.py
@@ -8,14 +8,14 @@ Original file is located at
 
 Importing the Dependencies
 """
-
+import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
-
+pip install streamlit
 """Data Collection and Analysis
 
 PIMA Diabetes Dataset
